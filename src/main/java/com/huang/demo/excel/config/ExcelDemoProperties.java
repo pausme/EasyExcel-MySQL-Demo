bd42782ec4ede642b96d05d438372a7a872e84ad
黄思豪 <sihao.huang@dyi-china.com>
2026-08-01 16:35:59 +0800

    [ADD] 修改环境变量取值逻辑

--- src/main/java/com/huang/demo/excel/config/ExcelDemoProperties.java
+++ src/main/java/com/huang/demo/excel/config/ExcelDemoProperties.java
@@ -1,59 +1,23 @@
 package com.huang.demo.excel.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "app.excel")
+@Getter
+@Setter
 public class ExcelDemoProperties {
 
-    private int exportPageSize = 500;
+    private int exportPageSize;
 
-    private int sheetRowLimit = 5000;
+    private int sheetRowLimit;
 
-    private int importBatchSize = 500;
+    private int importBatchSize;
 
-    private int insertBatchSize = 2000;
+    private int insertBatchSize;
 
-    private int demoSeedCount = 12000;
-
-    public int getExportPageSize() {
-        return exportPageSize;
-    }
-
-    public void setExportPageSize(int exportPageSize) {
-        this.exportPageSize = exportPageSize;
-    }
-
-    public int getSheetRowLimit() {
-        return sheetRowLimit;
-    }
-
-    public void setSheetRowLimit(int sheetRowLimit) {
-        this.sheetRowLimit = sheetRowLimit;
-    }
-
-    public int getImportBatchSize() {
-        return importBatchSize;
-    }
-
-    public void setImportBatchSize(int importBatchSize) {
-        this.importBatchSize = importBatchSize;
-    }
-
-    public int getInsertBatchSize() {
-        return insertBatchSize;
-    }
-
-    public void setInsertBatchSize(int insertBatchSize) {
-        this.insertBatchSize = insertBatchSize;
-    }
-
-    public int getDemoSeedCount() {
-        return demoSeedCount;
-    }
-
-    public void setDemoSeedCount(int demoSeedCount) {
-        this.demoSeedCount = demoSeedCount;
-    }
+    private int demoSeedCount;
 }
